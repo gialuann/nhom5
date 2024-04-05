21aea6c3a4e4b76e101c4ab8542c7ed5b651dadf
the-boys <ln08101996@gmail.com>
2024-04-05 11:41:02 +0700

    v3

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -5,19 +5,34 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Country;
 use App\Models\Mountain;
+use App\Models\MemberJoin;
+use App\Models\Join;
+
 class BlogController extends Controller
 {
     public function blog(){
         
         $country= Country::get();
+        $join=Join::get();
+        $memberjoin = MemberJoin::get();
+        $mountain = Mountain::get();
         return view('client.home.homepage',[
-            'countries' =>$country
+            'countries' =>$country,
+            'memberjoins'=>$memberjoin,
+            'joins'=>$join,
+            'mountains'=>$mountain
         ]);
     }
     public function head(){
         $country= Country::get();
+        $join=Join::get();
+        $memberjoin = MemberJoin::get();
+        $mountain = Mountain::get();
         return view('client.home.homepage',[
-            'countries' =>$country
+            'countries' =>$country,
+            'memberjoins'=>$memberjoin,
+            'joins'=>$join,
+            'mountains'=>$mountain
         ]);
     }
     
