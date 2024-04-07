06c0764fec23fab38edc2cc4265bb7c76e486c3e
Đặng Văn Thành Điệp <dangvanthanhdiep2711@gmail.com>
2024-04-07 23:22:38 +0700

    td23

--- resources/views/emails/test.blade.php
+++ resources/views/emails/test.blade.php
@@ -3,9 +3,20 @@
             margin: auto;
             padding: 15px;
             text-align: center;">
-    <p>Mountain</p>
-    <p>Date-Time</p>
-    <p>Information</p>
+    <table  border= "1"  cellpadding= "0"  cellspacing= "0"  width= "100%" >
+        <tr>
+          Hi {{tên người đăng kí}},Signed up for a climbing tour 
+        <tr>
+         <td  style= "padding: 20px 0 30px 0;" >
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tempus adipiscing felis, sit amet blandit ipsum volutpat sed. Morbi porttitor, eget accumsan dictum, nisi libero ultricies ipsum, in posuere mauris neque at erat.
+         </td>
+        </tr>
+        <tr>
+         <td>
+          Row 3
+         </td>
+        </tr>
+       </table>
    
 
 </div>
