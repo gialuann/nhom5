b5fcfd73935feaf81f1644bbc1d16c374c392f25
the-boys <ln08101996@gmail.com>
2024-04-08 14:45:18 +0700

    v40

--- resources/views/emails/test.blade.php
+++ resources/views/emails/test.blade.php
@@ -23,7 +23,7 @@
             text-align: center;">
     <table  border= "1"  cellpadding= "0"  cellspacing= "0"  width= "100%" >
         <tr>
-          Hi {{tên người đăng kí}},Signed up for a climbing tour 
+
         <tr>
          <td  style= "padding: 20px 0 30px 0;" >
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tempus adipiscing felis, sit amet blandit ipsum volutpat sed. Morbi porttitor, eget accumsan dictum, nisi libero ultricies ipsum, in posuere mauris neque at erat.
@@ -35,7 +35,5 @@
          </td>
         </tr>
        </table>
-   
->>>>>>> 06c0764fec23fab38edc2cc4265bb7c76e486c3e
 
 </div>
